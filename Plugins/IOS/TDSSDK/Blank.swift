40778bc1a169e1f758359caa7d9b24098712c3dd
肖熠 <xiaoyi@xindong.com>
2021-03-16 10:47:32 +0800

    del:删除IOS目录（改为iOS小写）

--- Plugins/IOS/TDSSDK/Blank.swift
+++
@@ -1,8 +0,0 @@
-//
-//  Blank.swift
-//  TestTDS
-//
-//  Created by JiangJiahao on 2020/10/28.
-//
-
-import Foundation
--- Plugins/IOS/TDSSDK/TDSGlobalSDKIAPKit.framework/Headers/TDSGlobalSDKIAPKit.h
+++
@@ -1,9 +0,0 @@
-
-#import <Foundation/Foundation.h>
-
-#import <TDSGlobalSDKIAPKit/TDSGlobalInAppPurchaseManager.h>
-#import <TDSGlobalSDKIAPKit/TDSGlobalProductInfo.h>
-#import <TDSGlobalSDKIAPKit/TDSGlobalTransactionInfo.h>
-#import <TDSGlobalSDKIAPKit/TDSGlobalOrderInfo.h>
-
-
