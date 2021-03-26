41b3a1ce740e9fb254e6e8b2bbcd21c355d39a95
xiaoyi <xiaoyi@xd.com>
2021-03-26 14:36:46 +0800

    build(android compile): fix android compille failed in Unity 2018

--- Script/Editor/TDSAndroidPostBuildProcessor.cs
+++ Script/Editor/TDSAndroidPostBuildProcessor.cs
@@ -8,7 +8,6 @@ using UnityEditor.Callbacks;
 using UnityEditor.Android;
 #endif
 using UnityEngine;
-
 using TDSEditor;
 
 public class TDSAndroidPostBuildProcessor : IPostGenerateGradleAndroidProject
@@ -17,15 +16,66 @@ public class TDSAndroidPostBuildProcessor : IPostGenerateGradleAndroidProject
     {
         get
         {
-            return 99;
+            return 999;
         }
+    }
+
+    public static bool GeneratedAndroidGradle(string projectPath)
+    {
+        if(!Directory.Exists(projectPath + "/launcher"))
+        {
+            string targetGradlePath = projectPath + "/build.gradle";
+            
+            string googleServiceOriginPath = projectPath + "/src/main/assets/google-services.json";
+
+            string targetServicePath = projectPath + "/google-services.json";
+
+            if(File.Exists(googleServiceOriginPath)){
+                //copy google-service.json 到目录下
+                Debug.Log($"Copy GoogleService.json to Android Project:{targetServicePath}");
+                File.Copy(googleServiceOriginPath,targetServicePath,true);
+            }
+
+            if(File.Exists(targetGradlePath))
+            {
+                TDSEditor.TDSScriptStreamWriterHelper writeHelper = new TDSEditor.TDSScriptStreamWriterHelper(targetGradlePath);
+                writeHelper.WriteBelow(@"apply plugin: 'com.android.application'",@"apply plugin: 'com.google.gms.google-services'");
+                writeHelper.WriteBelow(@"apply plugin: 'com.android.application'",@"buildscript {dependencies {classpath 'com.google.gms:google-services:4.0.2'}}");
+
+                writeHelper.WriteBelow(@"implementation fileTree(dir: 'libs', include: ['*.jar'])",@"
+                implementation 'com.google.firebase:firebase-core:16.0.1'
+                implementation 'com.google.firebase:firebase-analytics:15.0.1'
+                implementation 'com.google.firebase:firebase-messaging:17.3.4'
+                implementation 'com.google.code.gson:gson:2.8.6'
+                implementation 'com.google.android.gms:play-services-auth:16.0.1'
+                implementation 'com.facebook.android:facebook-login:5.15.3'
+                implementation 'com.facebook.android:facebook-share:5.15.3'
+                implementation 'com.appsflyer:af-android-sdk:4.11.0'
+                implementation 'com.adjust.sdk:adjust-android:4.24.1'
+                implementation 'com.android.installreferrer:installreferrer:2.1'
+                implementation 'com.android.billingclient:billing:3.0.0'
+                implementation 'com.android.support:support-annotations:28.0.0'
+                implementation 'com.android.support:appcompat-v7:28.0.0'
+                implementation 'com.android.support:recyclerview-v7:28.0.0'
+                implementation 'com.google.android.gms:play-services-ads-identifier:15.0.1'");
+                return true;
+            }else
+            {
+                Debug.LogError("TDSGlobal can't find Android Gradle File!");
+                return false;
+            }
+        }
+        return false;
     }
 
     void IPostGenerateGradleAndroidProject.OnPostGenerateGradleAndroidProject(string path)
     {   
-        Debug.Log("path:" + path);
-
         string projectPath = path.Substring(0,path.Length - 12);
+
+        if(GeneratedAndroidGradle(path))
+        {
+            return;
+        }
 
         string googleServiceOriginPath = projectPath + "/unityLibrary/src/main/assets/google-services.json";
 
@@ -68,7 +118,6 @@ public class TDSAndroidPostBuildProcessor : IPostGenerateGradleAndroidProject
         }
 
         if(File.Exists(unityLibraryGradle)){
-            Debug.Log("write unity gradle");
             TDSEditor.TDSScriptStreamWriterHelper writerHelper = new TDSEditor.TDSScriptStreamWriterHelper(unityLibraryGradle);
             writerHelper.WriteBelow(@"implementation fileTree(dir: 'libs', include: ['*.jar'])",@"
                 implementation 'com.google.firebase:firebase-core:16.0.1'
